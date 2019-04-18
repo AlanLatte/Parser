7cfe634890f2ec40f3423eda7cf44c223652efcc
AlanLatte <shukurovfoteh@gmail.com>
2019-04-19 02:20:53 +0300

    а ну

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def create_app():
     return render_template('index.html')
+
+@app.route('/search')
+def search_page():
+    return render_template('search.html')
+
+def search():
+    if request.method == 'POST':
+        if request.form['submit_button'] == 'submit':
+            print(request.args.get('data'))
 
 if __name__ == '__main__':
     app.run()
