b2f3411440d17e7b0181ba89b29fdafb33c3cd5a
AlanLatte <shukurovfoteh@gmail.com>
2019-04-29 18:00:30 +0300

    [*] Fixed __init__

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,10 @@ import os
 from flask import Flask, current_app
 
 from app.main import bp
-
+from config import Config
 def create_app():
     app = Flask(__name__)
-    # app.config.from_object(os.environ['APP_CONFIG'])
+    app.config.from_object(Config)
     app.register_blueprint(bp)
 
     return app
