d89ca6b638a0cf508380a774562064159d06b226
AlanLatte <shukurovfoteh@gmail.com>
2019-04-29 18:01:00 +0300

    [*] Fixed bug. Lost ")"

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 class Config(object):
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     CSRF_ENABLED = True
-    SECRET_KEY = os.urandom(16
+    SECRET_KEY = os.urandom(16)
     DATA_BASE_STORAGE = os.path.join(BASE_DIR, 'storage')
