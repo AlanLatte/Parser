2257b62b3d31c50d3b402f29ba232da1ae112bea
AlanLatte <shukurovfoteh@gmail.com>
2019-05-08 12:51:23 +0300

    [+] Set cookies algo init

--- app/main.py
+++ app/main.py
@@ -1,37 +1,31 @@
-from flask import  (Blueprint, render_template, request, g, redirect, jsonify,
-                    url_for, make_response, Response)
-from flask import current_app as app
+from flask          import  (Blueprint, render_template, request, g, redirect,
+                            url_for, make_response, Response, session)
+from flask          import current_app as app
 
-from app.parser import get_data as parser
+from app.parser     import get_data as parser
 
-from json import (load, dumps, loads)
-from os import listdir
-from time import time
+from json           import load
+from os             import listdir
+from time           import time
 
-bp          =   Blueprint('main', __name__, url_prefix='/', static_folder='/static')
+bp  =   Blueprint('main', __name__, url_prefix='/', static_folder='/static')
 
-urls        =   {
-                    'index'     :   '/',
-                    'search'    :   '/search',
-                    'result'    :   '/search/result',
-                    'team'      :   '/team'
-                }
-
-@bp.route(urls['index'])
+@bp.route('/')
 def index_page():
 
     return render_template('index.html')
 
-@bp.route(urls['search'], methods=["GET", "POST"])
+@bp.route('/search', methods=["GET", "POST"])
 def search_page():
-    response                 = make_response(redirect(url_for('.result_page')))
+    response                     = make_response(redirect(url_for('main.result_page')))
     # TODO: c++_java != java_c++ // NEED FIX
     if request.method == 'POST':
-        request_data         = request.json
+        request_data             = request.json
         if request_data:
             name                 =  '_'.join(sub_name for sub_name in request_data)
             DATA_BASE_STORAGE    = app.config['DATA_BASE_STORAGE']
             list_of_dir_DBS      = tuple(listdir(DATA_BASE_STORAGE))
+            response.set_cookie(key = "name", value = name)
             if f'{name}.json' in list_of_dir_DBS:
                 with open(f'{DATA_BASE_STORAGE}/{name}.json', encoding='utf8') as json_file:
                     data         = load(json_file)
@@ -42,21 +36,18 @@ def search_page():
                                     search_data     =   request_data,
                                     name            =   name
                                 )
-                        response.set_cookie(key = "name", value = name)
                         return response
                     else:
-                        response.set_cookie(key = "name", value = name)
                         return response
             else:
                 parser  (
                             search_data     =   request_data,
                             name            =   name
                         )
-                response.set_cookie(key = "name", value = name)
                 return response
-    return render_template('search.html', result_url=urls['result'])
+    return render_template('search.html', result_url='/search/result')
 
-@bp.route(urls['result'], methods=["GET", "POST"])
+@bp.route('/search/result')
 def result_page():
     # TODO: Work with POST api
     name = request.cookies.get("name")
@@ -64,9 +55,7 @@ def result_page():
     return f'<h1>{name}</h1>'
     # return redirect('/search'
 
-
-
-@bp.route(urls['team'])
+@bp.route('/team')
 def team_page():
     return render_template('team.html')
 
