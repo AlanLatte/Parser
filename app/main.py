e65708cd319bf5db579be148f219818d2b6d1fa4
AlanLatte <shukurovfoteh@gmail.com>
2019-05-19 19:18:06 +0300

    [+] Изменил POST на GET
    [*] Redirect fixed
    [+] Новая страница с парсингом по всем тэгам

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from flask          import (Blueprint, render_template, request, redirect,
-                                url_for, send_from_directory)
-from flask          import current_app as app
+                            url_for, send_from_directory)
+from flask          import current_app  as app
 
-from app.parser     import get_data as parser
+from app.parser     import get_data     as parser
+from app.all_tags   import all_tags
+
 
 bp  =   Blueprint('main', __name__, url_prefix='/', static_folder='/static')
 
@@ -14,15 +16,13 @@ def index_page():
 def search_page():
     return render_template('search.html')
 
-# TODO: изменить POST на GET
-# request.args.get flask в помощь.
-@bp.route('/request-result', methods=["POST"])
+@bp.route('/request-result', methods=["GET"])
 def request_result():
     """
         request_data : list()
         name         : str()
     """
-    request_data     = request.json
+    request_data     =  request.args.get('data').split(',')
     name             =  '_'.join(sub_name for sub_name in request_data)
     parser  (
                 search_data     =   request_data,
@@ -33,7 +33,21 @@ def request_result():
 @bp.route('/download-result/<path:file_name>', methods=["GET"])
 def download_page(file_name):
     if request.method == "GET":
-        return send_from_directory(app.config['DATA_BASE_STORAGE'], f'{file_name}.txt', as_attachment=False)
+        try:
+            return send_from_directory(app.config['DATA_BASE_STORAGE'], f'{file_name}.txt', as_attachment=True)
+        except:
+            return redirect(url_for('.main.search_page'))
+
+@bp.route('/search/all_tags', methods=["GET"])
+def all_tags_page():
+    name            = 'DATA_OF_ALL_TAGS'
+    FILE_OF_TAGS    = 'TAGS'
+    all_tags(
+                file_name = name,
+                FILE_OF_TAGS = FILE_OF_TAGS
+            )
+    return redirect(url_for('main.download_page', file_name=name, _method="GET"))
+
 
 @bp.route('/team', methods=["GET"])
 def team_page():
